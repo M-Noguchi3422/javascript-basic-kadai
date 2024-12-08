33c5f08f708f8d2416917b9fa220295f07cccff3
M-Noguchi3422 <starlight71712@gmail.com>
2024-12-08 23:11:58 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,9 @@
-let calculation = 68/(1.7*1.7);
+// 体重と身長の値を変数に代入
+let weight = 68; // 体重（kg）
+let height = 1.7; // 身長（m）
 
-console.log (calculation);
+// BMIを計算する
+let bmi = weight / (height * height);
+
+// 計算結果をコンソールに出力
+console.log(bmi);
